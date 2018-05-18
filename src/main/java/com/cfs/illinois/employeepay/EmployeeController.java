55162be847f0fb813a5794f1cb0e8ed4ec139e1c
TrueDoughnut <Christopher543sap@gmail.com>
2018-05-17 23:05:52 -0500

    Commit

--- src/main/java/com/cfs/illinois/employeepay/EmployeeController.java
+++ src/main/java/com/cfs/illinois/employeepay/EmployeeController.java
@@ -12,40 +12,37 @@ public class EmployeeController {
 
     private static final String tag = "/employee";
 
-    @RequestMapping(tag + "/id")
+    @RequestMapping(value = tag + "/id")
     public Object getEmployee(@RequestParam(value="id", required = false, defaultValue = "0") int id){
         return ReadILEmployeePay.employees.get(id);
     }
 
-    @RequestMapping(tag + "/name")
-    public Object getEmployee(@RequestParam(value = "name") String name){
-        int index = -1;
-        for (int i = 0; i < ReadILEmployeePay.employees.size(); i++) {
-            String bothName = null;
-            String[] singleName = null;
-            if(name.indexOf(" ") == -1){
-                singleName = ReadILEmployeePay.employees.get(i).getName().split(" ");
-            } else {
-                bothName = ReadILEmployeePay.employees.get(i).getName();
-            }
-            if(bothName != null){
-                if(bothName.equalsIgnoreCase(name)){
-                    index = i;
-                    break;
+    @RequestMapping(value = tag + "/name", params = "name")
+    public Object getEmployeeW(@RequestParam(value = "name") String name){
+        if(name.contains(" ")) {
+            for (int i = 0; i < ReadILEmployeePay.employees.size(); i++) {
+                if(ReadILEmployeePay.employees.get(i).getName().equalsIgnoreCase(name)){
+                    return ReadILEmployeePay.employees.get(i);
                 }
-            } else if(singleName != null){
-                for(String s : singleName){
-                    if(s.equalsIgnoreCase(name)){
-                        index = i;
-                        break;
+            }
+            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+        }
+        return getEmployeeWithSearch(name);
+    }
+
+    @RequestMapping(value = tag + "/name", params = "search")
+    public Object getEmployeeWithSearch(@RequestParam(value="search") String search){
+        String[] allSearchWords = search.split(" ");
+        for(int i = 0; i < ReadILEmployeePay.employees.size(); i++){
+            String[] data = ReadILEmployeePay.employees.get(i).getName().split(" ");
+            for(String str : allSearchWords){
+                for(String s : data){
+                    if(str.equalsIgnoreCase(s)){
+                        return ReadILEmployeePay.employees.get(i);
                     }
                 }
             }
         }
-        if (index >= 0) {
-            return ReadILEmployeePay.employees.get(index);
-        } else {
-            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-        }
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 }
